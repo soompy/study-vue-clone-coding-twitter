e510f89c02c0b3702911befbfc51fe8a5c3ff60a
KAKAOVX <>
2023-05-31 16:43:24 +0900

    홈 영역 추가

--- src/App.vue
+++ src/App.vue
@@ -63,7 +63,66 @@
                 </div>
             </div>            
         </div>
-        <div class="w-3/4">dd</div>
+        
+        
+        <!-- 메인 페이지 -->
+        <div class="flex-1">
+            <div class="flex h-screen">
+                <!-- 메인 섹션 -->
+                <div class="flex-1 border-r border-gray-100 overflow-y-auto">
+                    <div class="flex flex-col">
+                        <!-- 페이지 타이틀 -->
+                        <div class="border-b border-gray-100 px-3 py-2 font-bold text-lg">
+                            홈
+                        </div>
+                        <!-- 트위팅 섹션 -->
+                        <div class="flex px-3 py-3 border-b-8 border-gray-100">
+                            <img src="https://picsum.photos/200" class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer" />
+                            <div class="ml-2 flex-1 flex flex-col">
+                                <textarea class="w-full text-lg font-bold focus:outline-none mb-3 resize-none" name="" id="" cols="30" rows="10" placeholder="무슨일이 일어나고 있나요?"></textarea>
+                                <div class="text-right">
+                                    <button class="bg-primary hover:bg-dark text-sm font-bold text-white px-4 py-2 rounded-full">트윗</button>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- 트윗 -->
+                        <div class="flex px-3 py-3 border-b border-gray-100" v-for="tweet in 10" :key="tweet">
+                            <img src="https://picsum.photos/200" class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer" />
+                            <div class="ml-3 flex-1 flex flex-col">
+                                <div class="text-sm space-x-1">
+                                    <span class="font-bold">이수민</span>
+                                    <span class="text-gray-500 text-xs">@suri.l</span>
+                                    <span>*</span>
+                                    <span class="text-gray-500 text-xs">10분</span>
+                                </div>
+                                <!-- 트윗 바디 -->
+                                <div>트윗내용 트윗내용 트윗내용 트윗내용 트윗내용</div>
+                                <!-- 트윗 액션들 -->
+                                <div class="flex justify-between">
+                                    <div class="text-gray-500 hover:text-primary">
+                                        <i class="far fa-comment hover:bg-blue-50 rounded-full p-2"></i>
+                                        <span class="ml-1 text-sm">11</span>
+                                    </div>
+                                    <div class="text-gray-500 hover:text-green-500">
+                                        <i class="fa fa-retweet hover:bg-green-50"></i>                                    
+                                        <span class="ml-1 text-sm">11</span>
+                                    </div>
+                                    <div class="text-gray-500 hover:text-red-500">
+                                        <i class="far fa-heart hover:bg-red-50"></i>
+                                        <span class="ml-1 text-sm">11</span>
+                                    </div>
+                                    <div class="text-gray-500 hover:text-primary">
+                                        <i class="far fa-share-square"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- 트렌드 섹션 -->
+                <div></div>
+            </div>
+        </div>
     </div>
 </template>
 
