dfe2f2ca1e83593a6b951457cfc7b8b0efa40edb
KAKAOVX <>
2023-06-02 11:09:42 +0900

    [git add .눈으로 넘어가지 말것git add .] 로그인 상태에 따른 디폴트페이지, 최초화면(회원가입,로그인창) 구분

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="flex h-screen container mx-auto">
+        <!-- 사이드 섹션 -->
+        <div class="w-20 xl:w-1/4 pt-5 xl:ml-10 flex flex-col justify-between border-r border-gray-200">
+            <div class="flex flex-col items-center xl:items-start">
+                <i class="fab fa-twitter text-3xl text-primary xl:ml-4 mb-3"></i>
+                <!-- 트위터 사이드 메뉴 -->
+                <div class="flex flex-col items-start space-y-1">
+                    <router-link :to="route.path" class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer" v-for="route in routes" :key="route">
+                        <div v-if="route.meta.isMenu">
+                            <i :class="route.icon"></i>
+                            <span class="ml-3 text-lg hidden xl:inline-block">{{ route.title }}</span>
+                        </div>
+                        <div v-if="route.meta.isMenu"></div>                    
+                    </router-link>                    
+                </div>
+                <!-- 트윗 버튼 -->
+                <div class="w-full xl:pr-3 flex justify-center">
+                    <button class="mt-3 bg-primary text-white xl:w-full w-12 h-12 rounded-full hover:bg-dark">
+                        <span class="hidden xl:block">트윗</span>
+                        <i class="fas fa-plus xl:hidden"></i>
+                    </button>
+                </div>   
+            </div>
+            <!-- 프로필 버튼 -->
+            <div class="xl:pr-3 mb-3">
+                <button class="hidden xl:flex mt-3 px-2 py-1 w-full h-12 rounded-full hover:bg-blue-50 flex items-center">
+                    <!-- <img src="https://avatars.githubusercontent.com/u/31090387?v=4" class="w-10 h-10 rounded-full" /> -->
+                    <img src="https://picsum.photos/100" class="w-101₩ h-10 rounded-full" />
+                    <div class="xl:ml-2 hidden xl:block">
+                        <div class="text-sm font-bold">soomin</div>
+                        <div class="text-xs text-gray-500 text-left">@suri</div>
+                    </div>
+                    <i class="ml-auto fas fa-ellipsis-h fa-fw text-xs"></i>
+                </button>
+                <div class="xl:hidden flex justify-center">
+                    <img src="https://picsum.photos/100" class="w-10 h-10 rounded-full cursor-pointer hover:opacity-80" />
+                </div>
+            </div>            
+        </div>        
+        
+        <!-- 메인 페이지 -->
+        <div class="flex-1 flex h-screen">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+import { ref, onBeforeMount } from 'vue'
+import router from '../router'
+
+export default {
+    setup() {
+        const routes = ref([])
+
+        onBeforeMount(() => {
+            routes.value = router.options.routes
+        })
+
+        return {routes}
+    },
+}
+
+</script>
