72f78e20155a803c0db1b2609e404517d5445809
KAKAOVX <>
2023-06-02 14:16:57 +0900

    회원가입 페이지 작성

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,10 +1,37 @@
 <template>
-    <h1>register</h1>
+    <div class="flex flex-col items-center space-y-4 mt-10">
+        <i :class="`fab fa-twitter text-4xl text-primary ${loading ? 'animate-bounce' : '' }`"></i>
+        <span class="text-2xl font-bold">튓터 회원가입</span>
+        <input v-model="email" type="text" class="rounded w-96 px-4 py-4 border border-gray-300 focus:ring-2 focus:border-primary focus:outline-none" placeholder="이메일">
+        <input v-model="username" type="text" class="rounded w-96 px-4 py-4 border border-gray-300 focus:ring-2 focus:border-primary focus:outline-none" placeholder="아이디">
+        <input v-model="password" type="text" class="rounded w-96 px-4 py-4 border border-gray-300 focus:ring-2 focus:border-primary focus:outline-none" placeholder="비밀번호">
+        <button class="w-96 rounded bg-primary text-white py-4 hover:bg-dark" @click="onRegister">회원가입</button>
+        <button class="text-primary">계정이 이미 있으신가요? 로그인 하기</button>
+    </div>
 </template>
 
 <script>
-export default {
+import {ref} from 'vue'
 
+export default {
+    setup() {
+        const email = ref('')
+        const username = ref('')
+        const password = ref('')
+        const loading = ref(false)
+
+        const onRegister = (() => {
+            console.log(username.value, email.value, password.value)
+        })
+
+        return {
+            username,
+            email,
+            password,
+            loading,
+            onRegister
+        }
+    },
 }
 </script>
 
